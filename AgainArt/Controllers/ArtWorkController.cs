7036fc19694ef11b72c9bcffc3a26da01f1cf2c5
nanda13ap <nanda13ap@hotmail.com>
2018-01-25 20:00:03 -0200

    Tentando inserir artWork

--- AgainArt/Controllers/ArtWorkController.cs
+++ AgainArt/Controllers/ArtWorkController.cs
@@ -1,9 +1,13 @@
 ﻿using AgainArt.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace AgainArt.Controllers
 {
@@ -23,25 +27,95 @@ namespace AgainArt.Controllers
         [HttpPost]
         public ActionResult Add(HttpPostedFileBase file)
         {
-            ArtWork objArtWork = new ArtWork();
-            objArtWork.FileURL = file.FileName;
-
             if (ModelState.IsValid)
             {
-                MVCArtistContext db = new MVCArtistContext();
-                objArtWork.GeneratedName = objArtWork.OriginalName + "_" + Guid.NewGuid();
-                db.ArtWork.Add(objArtWork);
-                db.SaveChanges();
+
+
+
+                if (ModelState.IsValid)
+                {
+
+                    if (file != null)
+                    {
+                        string savedFileName = string.Empty;
+                        string savedThumbFile = string.Empty;
+
+                        try
+                        {
+                            ArtWork objArtWork = new ArtWork();
+                            //objArtWork.FileURL = file.FileName;
+                            objArtWork.OriginalName = file.FileName;
+                            objArtWork.IdArtista = 1;// na mao o codigo do artista
+                            objArtWork.GeneratedName = Path.GetFileNameWithoutExtension(objArtWork.OriginalName) + "_" + Guid.NewGuid() + Path.GetExtension(objArtWork.OriginalName);
+                            //objArtWork.ContentType = file.ContentType.ToString();
+
+
+                            //attach the uploaded image to the object before saving to Database
+                            //artwork.ImageMimeType = image.ContentLength;
+                            //artwork.ImageData = new byte[image.ContentLength];
+                            //image.InputStream.Read(artwork.ImageData, 0, image.ContentLength);
+
+                            //Save image to file
+                            //var filename = image.FileName;
+                            var filePathOriginal = Server.MapPath("/Content/ArtWorkImages/Original");
+                            var filePathThumbnail = Server.MapPath("/Content/ArtWorkImages/Thumbnail");
+                            savedFileName = Path.Combine(filePathOriginal, objArtWork.GeneratedName);
+                            savedThumbFile = Path.Combine(filePathThumbnail, objArtWork.GeneratedName);
+
+                            file.SaveAs(savedFileName);
+                            objArtWork.FileURL = savedFileName;
+
+                            //Read image back from file and create thumbnail from it
+                            var imageFile = savedFileName; //Path.Combine(Server.MapPath("~/Content/ArtWorkImages/Original"), objArtWork.GeneratedName);
+                            using (var srcImage = Image.FromFile(imageFile))
+                            using (var newImage = new Bitmap(100, 100))
+                            using (var graphics = Graphics.FromImage(newImage))
+                            using (var stream = new MemoryStream())
+                            {
+                                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                                graphics.DrawImage(srcImage, new Rectangle(0, 0, 100, 100));
+                                newImage.Save(stream, ImageFormat.Png);
+                                objArtWork.ThumbNailURL = savedThumbFile;
+                                //HttpPostedFileBase oi = newImage;
+                                newImage.Save(savedThumbFile);
+                                //var thumbNew = File(stream.ToArray(), "image/png");
+
+                                //artwork.ArtworkThumbnail = thumbNew.FileContents;
+
+
+                            }
+                            MVCArtistContext db = new MVCArtistContext();
+                            objArtWork.Artista = db.Artista.FirstOrDefault(a => a.Id == objArtWork.IdArtista);
+                            db.ArtWork.Add(objArtWork);
+                            db.SaveChanges();
+                        }
+                        catch (Exception ex)
+                        {
+                            if (System.IO.File.Exists(savedFileName))
+                            {
+                                System.IO.File.Delete(savedFileName);
+                            }
+
+                            if (System.IO.File.Exists(savedThumbFile))
+                            {
+                                System.IO.File.Delete(savedThumbFile);
+                            }
+
+
+                        }
+                    }
+                }
             }
             return View();
+
         }
 
-        [HttpPost]
+
         public ActionResult Add()
         {
-
-
-            return View();
+            return View("Index");
         }
 
         public ActionResult List(ArtWork objSearch = null)
---
+++ AgainArt/Migrations/201801252156598_novaQUadro.cs
@@ -0,0 +1,16 @@
+namespace AgainArt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class novaQUadro : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

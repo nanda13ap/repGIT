567a7b96260e57847c7d1266c18c59612f19a4e5
nanda13ap <nanda13ap@hotmail.com>
2018-01-28 11:55:39 -0200

    Atualizacao Contact.

--- AgainArt/Controllers/EmailController.cs
+++ AgainArt/Controllers/EmailController.cs
@@ -20,20 +20,29 @@ namespace AgainArt.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult SendEmail(Email emailContact)
+        public ActionResult SendEmail(Gallery gallery)
         {
+            Email objEmailContact = gallery.Email;
+
             SmtpClient client = new SmtpClient();
-            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"]);
 
             MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress(emailContact.From);
-            mailMessage.To.Add(ConfigurationManager.AppSettings["username"]);
-            mailMessage.Subject = emailContact.Subject;
-            mailMessage.Body = emailContact.Body;
+            mailMessage.From = new MailAddress(ConfigurationManager.AppSettings["username"], objEmailContact.Name);
+            //new MailAddress(objEmailContact.From);
+            mailMessage.To.Add(new MailAddress(ConfigurationManager.AppSettings["username"], objEmailContact.Name));
+            mailMessage.Subject = "Message from your Website: Paintings";
+            mailMessage.Body = string.Format("<h2>You have just received a message from your website.</h2><br><br> <b>Name:</b> {0}<br><b>Email:</b> {1}<br><b>Message: </b> {2} ", objEmailContact.Name, objEmailContact.From, objEmailContact.Body);
+            mailMessage.Sender = new MailAddress(objEmailContact.From, objEmailContact.Name);
+            mailMessage.IsBodyHtml = true;
+
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"]);
 
             client.Send(mailMessage);
 
-            return View("Index");
+            return RedirectToAction("Index", "Gallery");
 
         }
     }

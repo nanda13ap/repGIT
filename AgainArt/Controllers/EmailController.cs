6ec96d2a33e785e87a251cf4df7be88407539f4f
nanda13ap <nanda13ap@hotmail.com>
2018-02-13 21:58:20 -0200

    adjustments

--- AgainArt/Controllers/EmailController.cs
+++ AgainArt/Controllers/EmailController.cs
@@ -32,10 +32,10 @@ namespace AgainArt.Controllers
                 MailMessage mailMessage = new MailMessage();
                 mailMessage.From = new MailAddress(ConfigurationManager.AppSettings["username"], objEmailContact.Name);
                 //new MailAddress(objEmailContact.From);
-                mailMessage.To.Add(new MailAddress(ConfigurationManager.AppSettings["username"], objEmailContact.Name));
+                mailMessage.To.Add(mailMessage.From.Address);
                 mailMessage.Subject = "Message from your Website: Paintings";
                 mailMessage.Body = string.Format("<h2>You have just received a message from your website.</h2><br><br> <b>Name:</b> {0}<br><b>Email:</b> {1}<br><b>Message: </b> {2} ", objEmailContact.Name, objEmailContact.From, objEmailContact.Body);
-                mailMessage.Sender = new MailAddress(objEmailContact.From, objEmailContact.Name);
+                mailMessage.Sender = new MailAddress(mailMessage.From.Address, objEmailContact.Name);
                 mailMessage.IsBodyHtml = true;
 
                 client.DeliveryMethod = SmtpDeliveryMethod.Network;
@@ -47,7 +47,7 @@ namespace AgainArt.Controllers
 
                 Success(string.Format("The information was successfully saved in the database."), true);
             }
-            catch
+            catch (Exception ex)
             {
                 Danger("It looks like something went wrong. Please try again later.");
             }
